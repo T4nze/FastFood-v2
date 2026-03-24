b5d0f32d3ae067d2325f41dd4262ccd1a62df78e
T4nze <stathispap208@gmail.com>
2026-03-24 18:52:12 +0200

    Pushed

--- src/FrontPageUI.java
+++ src/FrontPageUI.java
@@ -1,2 +1,22 @@
-public class FrontPageUI {
+import javax.swing.*;
+
+public class FrontPageUI extends JFrame {
+
+    private JPanel panel1;
+    private JTextField textField1;
+
+    public FrontPageUI() {
+        setTitle("Jakaroo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new FrontPageUI();
+
+    }
+
 }
